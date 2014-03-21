eadeefe4e6b89a7709b817d3c3e940dc54715111
Dave Potts <dave@goopot.co.uk>
2014-03-21 21:08:36 +0000

    pylint updates

--- rube.py
+++ rube.py
@@ -1,26 +1,36 @@
-
+"""Code to manage the Rube Goldberg project for the Manchester CoderDojo """
 
 
 
 class Source(object):
-    """A source object represents the part of the machine that will trigger the next step when it completes.
+    """A source object represents the part of the machine that will trigger the 
+	next step when it completes.
     
-    A source adaptor is responsible for responding to the pollState() method by returning the current state of the source part of the machine.
+    A source adaptor is responsible for responding to the poll_state() method by 
+	returning the current state of the source part of the machine.
     
     """
     
-    def pollState(self):
-        raise NotImplementedError("It is for concrete implementations of this class to fill in the pollState() method.")
+    def poll_state(self):
+        """Ask a source component for its current state"""
+        raise NotImplementedError(
+		"It is for concrete implementations of this class to fill in the" \
+		"poll_state() method.")
         
         
         
 class Target(object):
     """
-    A target object represents the part of the machine that should be triggered to start of the next step running.
+    A target object represents the part of the machine that should be triggered
+	to start of the next step running.
     
-    A target adaptor is responsible for responding to the updateState(block) method by taking appropriate actions to update its state.
+    A target adaptor is responsible for responding to the update_state(block) 
+	method by taking appropriate actions to update its state.
     
     """
     
-    def updateState(self, block):
-        raise NotImplementedError("It is for concrete implementations of this class to fill in the updateState(block) method.")
+    def update_state(self, block):
+        """Ask the target component to set its state to the passed Minecraft 
+        Block object value."""
+        raise NotImplementedError("It is for concrete implementations of this" \
+			"class to fill in the update_state(block) method.")
--- test_rube.py
+++ test_rube.py
@@ -1,29 +1,29 @@
 import rube
-import unittest
+import  
 
 
 class TestRube(unittest.TestCase):
 
-	def test_pollState(self):
-		source = MockSource()
-		source.state = 1
-		self.assertEqual(source.pollState(), 1)
-		
-	def test_updateState(self):
-		target = MockTarget()
-		target.updateState(1)
-		self.assertEqual(target.last_state_update, 1)
-		
+    def test_poll_state(self):
+        source = MockSource()
+        source.state = 1
+        self.assertEqual(source.poll_state(), 1)
+        
+    def test_update_state(self):
+        target = MockTarget()
+        target.update_state(1)
+        self.assertEqual(target.last_state_update, 1)
+        
 class MockSource(rube.Source):
-	
-	def pollState(self):
-		return 1
-		
-		
+    
+    def poll_state(self):
+        return 1
+        
+        
 class MockTarget(rube.Target):
-	
-	def updateState(self, block):
-		self.last_state_update = block
+    
+    def update_state(self, block):
+        self.last_state_update = block
 
-	
-	
+    
+    
