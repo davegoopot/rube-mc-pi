14993139181aa8b58d8488b3f33b13f1b09b6e60
davegoopot <dave@goopot.co.uk>
2014-03-22 11:04:28 +0000

    Update target to polled value

--- rube.py
+++ rube.py
@@ -2,48 +2,49 @@
 
 
 class RubeController(object):
-	""""Responsible for controlling the interactions between components in
-	the Rube Goldberg machine.
-	"""
+    """"Responsible for controlling the interactions between components in
+    the Rube Goldberg machine.
+    """
 
-	def __init__(self, config):
-		"""Set up a controller using the passed config.
+    def __init__(self, config):
+        """Set up a controller using the passed config.
 
-		The config is a tuple of (source, target) pairs.
-		"""
-		self.config = config
+        The config is a tuple of (source, target) pairs.
+        """
+        self.config = config
 
 
-	def update_all_once(self):
-		"""Run through all the config pairs once checking state and updating """
-		
-		for (source, target) in self.config:
-			source.poll_state()
+    def update_all_once(self):
+        """Run through all the config pairs once checking state and updating """
+        
+        for (source, target) in self.config:
+            new_state = source.poll_state()
+            target.update_state(new_state)
 
 class Source(object):
     """A source object represents the part of the machine that will trigger the 
-	next step when it completes.
-    
+    next step when it completes.
+
     A source adaptor is responsible for responding to the poll_state() method by 
-	returning the current state of the source part of the machine.
+    returning the current state of the source part of the machine.
     
     """
     
     def poll_state(self):
         """Ask a source component for its current state"""
         raise NotImplementedError(
-		"It is for concrete implementations of this class to fill in the" \
-		"poll_state() method.")
+        "It is for concrete implementations of this class to fill in the" \
+        "poll_state() method.")
         
         
         
 class Target(object):
     """
     A target object represents the part of the machine that should be triggered
-	to start of the next step running.
+    to start of the next step running.
     
     A target adaptor is responsible for responding to the update_state(block) 
-	method by taking appropriate actions to update its state.
+    method by taking appropriate actions to update its state.
     
     """
     
@@ -51,4 +52,4 @@ class Target(object):
         """Ask the target component to set its state to the passed Minecraft 
         Block object value."""
         raise NotImplementedError("It is for concrete implementations of this" \
-			"class to fill in the update_state(block) method.")
+            "class to fill in the update_state(block) method.")
--- test_rube.py
+++ test_rube.py
@@ -16,23 +16,33 @@ class TestRube(unittest.TestCase):
 		self.target.update_state(1)
 		self.assertEqual(self.target.last_state_update, 1)
 	
-	def test_event_loop(self):
+	def test_event_loop_calls_poll(self):
 		source2 = MockSource()
-		config = ( (self.source, self.target), (source2, None)  )
+		target2 = MockTarget()
+		config = ( (self.source, self.target), (source2, target2)  )
 		controller = rube.RubeController(config)
 		controller.update_all_once()
 		self.assertTrue(self.source.was_poll_state_called)
 		self.assertTrue(source2.was_poll_state_called)
 	
+	def test_event_loop_calls_update_on_change(self):
+		self.source._state = 1
+		config = ( (self.source, self.target),  )
+		controller = rube.RubeController(config)
+		controller.update_all_once()
+		self.source._state = 2
+		controller.update_all_once()
+		self.assertEquals(self.target.last_state_update, 2)
         
 class MockSource(rube.Source):
     
     def __init__(self):
     	self.was_poll_state_called = False
+    	self._state = 1
     
     def poll_state(self):
     	self.was_poll_state_called = True
-        return 1
+        return self._state
         
         
 class MockTarget(rube.Target):
