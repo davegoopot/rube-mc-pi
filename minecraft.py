4c60611fffac3ce44d37c9e18771c47d6cbc67d0
Dave Potts <dave@goopot.co.uk>
2014-04-06 09:32:10 +0100

    Refactored out repeated code for setting up the link to the minecraft server

--- minecraft.py
+++ minecraft.py
@@ -15,19 +15,13 @@ class MinecraftSource(rube.Source): #pylint: disable=R0903
     """Monitor a minecraft server for the block state at given coordinates"""
     def __init__(self, attribs):
         super(MinecraftSource, self).__init__()
-        self.server_address = attribs["server_address"]
-        self.server_port = attribs.get("server_port", 4711)
-        self.coords_x = attribs["coords_x"]
-        self.coords_y = attribs["coords_y"]
-        self.coords_z = attribs["coords_z"]
-        self.world_connection = Minecraft.create(self.server_address,
-                                                 self.server_port)
+        self.mc_link = MinecraftLink(attribs)
 
     def poll_state(self):
         """Return the block value from the connected Minecraft server"""
-        block_ = self.world_connection.getBlock(self.coords_x,
-                                               self.coords_y,
-                                               self.coords_z)
+        block_ = self.mc_link.world_connection.getBlock(self.mc_link.coords_x,
+                                                        self.mc_link.coords_y,
+                                                        self.mc_link.coords_z)
         print "Got " + str(block_)
         return block_
 
@@ -35,21 +29,30 @@ class MinecraftTarget(rube.Target): #pylint: disable=R0903
     """Set the block on the server to the specified value"""
     def __init__(self, attribs):
         super(MinecraftTarget, self).__init__()
+        self.mc_link = MinecraftLink(attribs)
+
+    def update_state(self, block_):
+        self.mc_link.world_connection.setBlock(self.mc_link.coords_x,
+                                               self.mc_link.coords_y,
+                                               self.mc_link.coords_z,
+                                               block_)
+
+                                       
+class MinecraftLink(object):
+    """
+    Responsible for storing the state of the link to a server and the location
+    of the block that is to be monitored or updated.
+    """
+                                       
+    def __init__(self, attribs):
         self.server_address = attribs["server_address"]
         self.server_port = attribs.get("server_port", 4711)
         self.coords_x = attribs["coords_x"]
         self.coords_y = attribs["coords_y"]
         self.coords_z = attribs["coords_z"]
         self.world_connection = Minecraft.create(self.server_address,
-                                                 self.server_port)
-
-    def update_state(self, block_):
-        self.world_connection.setBlock(self.coords_x,
-                                       self.coords_y,
-                                       self.coords_z,
-                                       block_)
-
-
+                                                 self.server_port)                                       
+                                       
 if __name__ == "__main__":
     JSON_CONFIG = ""
     with open("config.json", "r") as f:
@@ -57,9 +60,9 @@ if __name__ == "__main__":
     
     CONFIG = rube.ConfigJsonParser.parse(JSON_CONFIG)
     SOURCE = CONFIG[0][0]
-    SOURCE.world_connection.setBlock(1,
-                                     10,
-                                     3,
-                                     block.TNT)
+    SOURCE.mc_link.world_connection.setBlock(1,
+                                             10,
+                                             3,
+                                             block.TNT)
     CONTROLLER = rube.RubeController(CONFIG)
     CONTROLLER.run_event_loop()
