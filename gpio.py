6ee8a39023a304981b97122b85781c24a002aee3
Dave Potts <dave@goopot.co.uk>
2014-04-09 09:10:48 +0100

    Not working version of GPIO need to work out how to mock

--- gpio.py
+++ gpio.py
@@ -18,18 +18,29 @@ The JSON config looks like this:
 """
 
 from mcpi.block import Block
+import RPi.GPIO as GPIO
 import rube
+
+
 
 class GpioSource(rube.Source): #pylint: disable=R0903
     """
         Use the input from the raspberry pi GPIO pin 
     """
+    
+    @staticmethod
+    def gpio_in_setup(pin):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    
     def __init__(self, attribs):
         super(GpioSource, self).__init__()
         self.pin = attribs["pin"]
         self.low_state_block = Block(attribs["low_state_block"][0], attribs["low_state_block"][1])
         self.high_state_block = Block(attribs["high_state_block"][0], attribs["high_state_block"][1])
+        gpio_in_setup(self.pin)
 
-        def poll_state(self):
-            pass
+    def poll_state(self):
+        pass
 
