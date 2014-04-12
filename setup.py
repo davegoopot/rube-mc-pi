73dd8c39549204140663819037e4be4e83cfafba
davegoopot <dave@goopot.co.uk>
2014-04-12 11:21:33 +0100

    Moving ready for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rube_mc_pi',
+      version='0.1',
+      description='Controller code for the Manchester CoderDojo Rube Goldberg Proejct',
+      url='https://github.com/davegoopot/rube-mc-pi',
+      author='Dave Potts',
+      author_email='dave@goopot.co.uk',
+      license='GPL2',
+      packages=['rube_mc_pi'],
+      zip_safe=False)
