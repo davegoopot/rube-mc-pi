08be1c14fc46fc346c81e25689bac532bf8c9c87
Dave Potts <dave@goopot.co.uk>
2014-05-10 14:17:40 +0100

    Fixing pylint

--- rube_mc_pi/playerloc.py
+++ rube_mc_pi/playerloc.py
@@ -9,18 +9,17 @@ usage:  python playerloc.py [server-name] [port]
 
 import rube_mc_pi.mcpi.minecraft as minecraft
 import sys
-import time
 
 
 if __name__ == "__main__":
-    servername = "localhost"
-    port = 4711
+    SERVERNAME = "localhost"
+    PORT = 4711
     if len(sys.argv) > 1:
-        servername = sys.argv[1]
+        SERVERNAME = sys.argv[1]
     if len(sys.argv) > 2:
-        port = int(sys.argv[2])
-    print("Opening world '%s:%d'" % (servername, port))
-    world = minecraft.Minecraft.create(servername, port)
+        PORT = int(sys.argv[2])
+    print("Opening world '%s:%d'" % (SERVERNAME, PORT))
+    WORLD = minecraft.Minecraft.create(SERVERNAME, PORT)
     print("Connected to world")
-    position = world.player.getPos()
-    print("Player is at (%d, %d, %d)" % (position.x, position.y, position.z))
+    POSITION = WORLD.player.getPos()
+    print("Player is at (%d, %d, %d)" % (POSITION.x, POSITION.y, POSITION.z))
