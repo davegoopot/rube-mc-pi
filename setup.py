1d64d82791ec880a74ab0f22eeaae52562881b89
Dave Potts <dave@goopot.co.uk>
2014-06-07 15:28:21 +0000

    Version update to get minecraft shared connection fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import multiprocessing
 from setuptools import setup
 
 setup(name='rube_mc_pi',
-      version='0.2.1',
+      version='0.2.2',
       description='Controller code for the Manchester CoderDojo Rube Goldberg Proejct',
       url='https://github.com/davegoopot/rube-mc-pi',
       author='Dave Potts',
